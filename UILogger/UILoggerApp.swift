213522f5e94c41065f0c847190eb2289fce908a4
Omer Hamid Kamisli <me@ohkamisli.dev>
2023-04-22 20:36:30 -0700

    Initial Commit

---
+++ UILogger/UILoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UILoggerApp.swift
+//  UILogger
+//
+//  Created by Ömer Hamid Kamışlı on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct UILoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
